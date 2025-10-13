f32f88efc4b125c5d2ca24211e8d1e542277dc14
mozow01 <78300414+mozow01@users.noreply.github.com>
2025-10-13 14:50:18 +0200

    Add files via upload

--- stlc.v
+++ stlc.v
@@ -1,18 +1,29 @@
+(*Curry--Howard-izomorfizmus:
+      logika   =   funcionális típusos programozás 
+      
+      Egyszerű típuselmélet
+*)
+
+(* Γ ⊢ A *)
+
+(* Γ ⊢ p : A, 
+
+Γ ⊢ p : A → B, 
+  
+  x : A ⊢ p : B ;  ⊢ λ(x:A).p : A → B  *)
+
 Require Import List.
 Import ListNotations.
 
 Inductive Typ : Set :=
   | At : nat -> Typ
-  | Uni : Typ
+  | Tru : Typ
   | Imp : Typ -> Typ -> Typ
   | Cnj : Typ -> Typ -> Typ.
-  
-  (* Γ ⊢ p : A, 
-  
-  x : C ⊢ p : A ,  ⊢ lambda x:C.p : C → A *)
+ 
   
 Inductive Trm : Set :=
-  | uni : Trm
+  | tru : Trm
   | hyp : nat -> Trm
   | lam : Typ -> Trm -> Trm
   | app : Trm -> Trm -> Trm
@@ -22,11 +33,11 @@ Inductive Trm : Set :=
   
 Definition Cntxt := list Typ.
 
-Print list_rect.
+Print nat_ind.
 
 
 Inductive Tyty : Cntxt -> Trm -> Typ -> Prop :=
-  | STT_uni : forall Γ, Tyty Γ uni Uni
+  | STT_tru : forall Γ, Tyty Γ tru Tru
   | STT_hyp0 : forall Γ A, Tyty (A :: Γ) (hyp 0) A
   | STT_hypS :
       forall Γ A B i,
@@ -51,11 +62,16 @@ Notation "Γ '⊢' t '[:]' A" := (Tyty Γ t A) (at level 70, no associativity) :
 
 Notation "'⊢' t '[:]' A" := (Tyty nil t A) (at level 70, no associativity) : type_scope.
 
+Print list.
+
 Notation "A ⊃ B" := (Imp A B) (at level 70, right associativity) :
 type_scope.
 
+Notation "A ∧ B" := (Cnj A B) (at level 70, right associativity) :
+type_scope.
 
-Lemma problem_1 : forall A : Prop, A -> A.
+(*Az igaz, az igaz.*)
+Lemma Native_I_combinator : forall A : Prop, A -> A.
 Proof.
 intro A. 
 intro x.
@@ -67,13 +83,13 @@ Qed.
 
 x: A ⊢ x [:] A              ;   A ⊢ hyp 0 [:] A 
 ------------------          ; 
- x: A ⊢ ? [:] A             ;
+ x: A ⊢ x [:] A             ;
 -----------------------     ;  
- ⊢ ? [:] (A ⊃ A)            ;     ⊢ lam A (hyp 0) [:] (A ⊃ A)
+ ⊢  [:] (A ⊃ A)             ;     ⊢ lam A (hyp 0) [:] (A ⊃ A)
 
   *)
 
-Lemma indentity : forall A : Typ, exists t, ⊢ t [:] (A ⊃ A).
+Lemma I_combinator : forall A : Typ, exists t, ⊢ t [:] (A ⊃ A).
 Proof.
 intros.
 exists (lam A (hyp 0)).
@@ -81,7 +97,82 @@ apply STT_lam.
 apply STT_hyp0.
 Qed.
 
-Lemma problem_2 : forall A B C : Prop, (A -> B) -> ((B -> C) -> (A -> C)).
+Lemma Native_K_combinator : forall A B, A -> B -> A.
+Proof.
+intros A B.
+intros x.
+intros y.
+exact x.
+Show Proof.
+Defined.
+
+
+(*  de Bruijn kódolás ( névtelen változók )
+
+
+sin x 
+
+x |-----> sin x 
+
+                                           A ⊢ hyp 0 [:] A 
+
+y: B, x: A ⊢ y [:] A              ;   [B, A] ⊢ hyp 1 [:] A  
+------------------          ; 
+ x: A ⊢ ? [:] B ⊃ A            ;      [A] ⊢ lam B (hyp 1) [:] B ⊃ A 
+-----------------------     ;  
+  ⊢ ? [:] (A ⊃ (B ⊃ A)).            ;     ⊢ lam A (lam B (hyp 1)) [:] (A ⊃ (B ⊃ A))
+
+  *)
+
+Lemma K_combinator : forall A B : Typ, exists t, ⊢ t [:] (A ⊃ (B ⊃ A)).
+Proof.
+intros A B.
+exists (lam A (lam B (hyp 1))).
+apply STT_lam.
+apply STT_lam.
+apply STT_hypS.
+apply STT_hyp0.
+Qed.
+
+Lemma Native_conj_comm: forall A B : Prop, A /\ B -> B /\ A.
+Proof.
+intros A B. 
+intro x.
+destruct x as [x1 x2].
+split.
+exact x2.
+exact x1.
+Show Proof.
+Qed. 
+
+
+
+(*
+
+x : (conj A B)  ⊢ hyp 0 [:] (conj A B)       x : (conj A B)  ⊢ hyp 0 [:] (conj A B)
+-----------------------------------        -------------------------------
+x : (conj A B)  ⊢  proj_2 (hyp 0) [:] B          x : (conj A B)  ⊢  proj_1 (hyp 0)  [:]  A
+---------------------------------------------------------------------------------- 
+x : (conj A B)  ⊢ cnj (proj_2 (hyp 0)) (proj_1 (hyp 0))  [:] (conj B A)
+-----------------------    
+ ⊢ (lam (conj A B) (cnj (proj_2 (hyp 0)) (proj_1 (hyp 0)))) [:] (conj A B) ⊃ (conj B A)
+
+  *)
+
+Lemma Cnj_comm : forall A B : Typ, exists t, ⊢ t [:] ((A ∧ B) ⊃ (B ∧ A)).
+Proof.
+intros.
+exists (lam (Cnj A B) (cnj (proj_2 (hyp 0)) (proj_1 (hyp 0)))).
+apply STT_lam.
+apply STT_cnj.
+apply STT_proj2 with (A:=A).
+apply STT_hyp0.
+apply STT_proj1 with (B:=B).
+apply STT_hyp0.
+Defined.
+
+
+Lemma Native_S_combinator: forall A B C : Prop, (A -> B) -> ((B -> C) -> (A -> C)).
 Proof.
 intros A B C. 
 intro x.
@@ -108,7 +199,7 @@ y: (B ⊃ C), x: (A ⊃ B) ⊢ (lam A (app (hyp 1) (app (hyp 2) (hyp 0)))) [:] (
 
   *)
 
-Lemma chain : forall A B C : Typ, exists t, ⊢ t [:] ((A ⊃ B) ⊃ ((B ⊃ C) ⊃ (A ⊃ C))).
+Lemma S_combinator : forall A B C : Typ, exists t, ⊢ t [:] ((A ⊃ B) ⊃ ((B ⊃ C) ⊃ (A ⊃ C))).
 Proof.
 intros.
 exists ((lam (A ⊃ B)  (lam (B ⊃ C) (lam A (app (hyp 1) (app (hyp 2) (hyp 0))))))).
